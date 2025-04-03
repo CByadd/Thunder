1c66b696df898dd8fbafd7f7e1c4d2ee687bffc6
Srinivash Dev <82706452+CByadd@users.noreply.github.com>
2025-04-03 11:20:21 +0530

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import './../styles/Landing.css';
-const Landing = () => {
-  return (
-    <>
-    
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./../styles/Landing.css";
 
+const Landing = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="landing-container">
       <div className="buttons">
-        <button className="start-button">Get Started</button>
+        <button className="start-button" onClick={() => navigate("/register")}>
+          Register
+        </button>
+        <button className="start-button" onClick={() => navigate("/login")}>
+          Login
+        </button>
         <div className="block1"></div>
         <div className="block2"></div>
         <div className="block3"></div>
         <div className="block4"></div>
-        <p className="green"> The Sneaker Spot</p>
+        <p className="green">The Sneaker Spot</p>
         <div className="content-boxs">
           <p className="contents">
-            encompass the visual appeal and style choices related to footwear,
+            Encompass the visual appeal and style choices related to footwear,
             ranging from classic designs to unique, personalized styles, often
-            reflecting personal expression and trends
+            reflecting personal expression and trends.
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
-export default Landing
+export default Landing;
